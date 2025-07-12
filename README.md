4226ea11c5540bf14a260d41390abaf516024e42
Alejandra Baudi <alejandrabaudi2011@gmail.com>
2025-07-12 18:09:32 -0300

    Agrego mi proyecto con README y captura de pantalla

---
+++ README.md
@@ -0,0 +1,36 @@
+# Frases Motivacionales 
+Esta es una página web simple que te da una frase motivacional aleatoria usando tu nombre, haciéndola un poco más personalizada.
+
+---
+
+## ¿Cómo funciona?
+1. Ingresás tu nombre. 
+2. Muestra una frase motivadora junto a tu nombre ingresado anteriormente. 
+3. Podes volver atrás para ingresar otro nombre. 
+
+---
+
+## Tecnologías usadas
+- HTML
+- CSS
+- JavaScript
+
+---
+
+ ## Cómo usar
+ 1. Cloná este repositorio o descargalo:
+    ```bash
+    git clone https://github.com/AlejandraBaudi/Frases-Motivacionales.git
+    ```
+ 2. Abrí el archivo 'index.html' en tu navegador.
+ 3. Listo! Empeza a usarla para motivarte cuando lo necesites. 
+
+ ---
+
+ ## Hecho por 
+ Alejandra Leonor Baudi 
+
+ ---
+
+ ## Captura de pantalla
+ ![Captura de pantalla](screenshot.png)
---
+++ script.js
@@ -0,0 +1,58 @@
+const frases = [
+  "Cree en ti y todo será posible.",
+  "Cada día es una nueva oportunidad.",
+  "Nunca es tarde para empezar de nuevo.",
+  "Lo mejor está por venir.",
+  "Sé la energía que querés atraer.",
+  "El éxito es la suma de pequeños esfuerzos repetidos día tras día.",
+  "No te rindas, lo bueno tarda.",
+  "Convertí los obstáculos en oportunidades.",
+  "Confía en tu proceso.",
+  "Hoy es un buen día para empezar."
+];
+
+const welcomeScreen = document.querySelector(".pantalla-bienvenida");
+const fraseScreen = document.querySelector(".pantalla-frase");
+
+const nombreInput = document.getElementById("nombre");
+const comenzarBtn = document.getElementById("btn-comenzar");
+const nombreUsuarioSpan = document.getElementById("nombre-usuario");
+const fraseElemento = document.getElementById("frase");
+const volverBtn = document.getElementById("btn-volver");
+
+let nombreUsuario = "";
+
+// Al hacer clic en "Comenzar"
+comenzarBtn.addEventListener("click", () => {
+  const nombre = nombreInput.value.trim();
+
+  if (nombre === "") {
+    alert("Por favor, ingresá tu nombre.");
+    return;
+  }
+
+  nombreUsuario = nombre;
+  nombreUsuarioSpan.textContent = nombreUsuario;
+
+  welcomeScreen.style.display = "none";
+  fraseScreen.style.display = "block";
+
+  mostrarNuevaFrase();
+});
+
+// Función para mostrar frase personalizada
+function mostrarNuevaFrase() {
+  const indice = Math.floor(Math.random() * frases.length);
+  const frase = frases[indice];
+  fraseElemento.textContent = `${nombreUsuario}, ${frase}`;
+}
+
+
+volverBtn.addEventListener("click", () => {
+  // Limpiar el campo de nombre y volver a mostrar la pantalla inicial
+  nombreInput.value = "";
+  fraseElemento.textContent = "Click acá para encontrar tu frase del día";
+
+  fraseScreen.style.display = "none";
+  welcomeScreen.style.display = "block";
+});
